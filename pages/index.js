373dd611bf0828d9a9ab8b38a2a628bb9dd54101
Welliton Klein <welliton.fokushima@gmail.com>
2023-08-08 01:14:18 +0000

    refactor: add aspas na citacao

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O sucesso é constituído por 10% de inspiração e 90% de transpiração - Thomas Edison</h1>
+    return <h1>"O sucesso é constituído por 10% de inspiração e 90% de transpiração" - Thomas Edison</h1>
 }
 
 export default Home;
