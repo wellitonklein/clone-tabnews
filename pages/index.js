04dc2b9893fc2f30bb1590fca82efe641fde2e5b
Welliton Klein <welliton.fokushima@gmail.com>
2023-08-23 02:01:14 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>A garotinha mais incrível do mundo!! ❤‍🔥❤‍🔥</h1>
+  return <h1>A garotinha mais incrível do mundo!! ❤‍🔥❤‍🔥</h1>;
+}
+
+function test() {
+  return <h1>Teste A!! ❤‍🔥❤‍🔥</h1>;
 }
 
 export default Home;
