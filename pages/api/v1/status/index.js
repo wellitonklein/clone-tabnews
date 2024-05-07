fe53796f8b90b5b083aaab58b39016102b80f170
wellitonklein <welliton.fokushima@gmail.com>
2024-05-06 22:31:36 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+
+async function status(request, response) {
+    const result = await database.query("SELECT 1 + 1;");
+    console.log(result.rows);
     response
         .status(200)
         .json({ "chave": "alunos do curso.dev sao acima da média" });
