fadf9d20d01614df441c32e71a26f7f993427dc6
wellitonklein <welliton.fokushima@gmail.com>
2024-05-07 23:52:44 -0300

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -1,37 +1,37 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        ssl: process.env.NODE_ENV !== "development",
-    });
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
 
-    console.log("Credenciais do Postgres: ", {
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        ssl: process.env.NODE_ENV !== "development",
-    });
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
 
-    try {
-        await client.connect();
-        const result = await client.query(queryObject);
-        return result;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    } finally {
-        await client.end();
-    }
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
 
+  return process.env.NODE_ENV !== "development";
 }
 
 export default {
-    query: query
+  query: query,
 };
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,31 +1,34 @@
 import database from "infra/database.js";
 import { version } from "react";
 
-
 async function status(request, response) {
-    const updateAt = new Date().toISOString();
+  const updateAt = new Date().toISOString();
 
-    const databaseVersionResult = await database.query("SHOW server_version;");
-    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-    const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
-    const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
-    const databaseName = process.env.POSTGRES_DB;
-    const databaseOpenedConnectionsResult = await database.query({
-        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-        values: [databaseName]
-    });
-    const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
-    response.status(200).json({
-        update_at: updateAt,
-        dependencies: {
-            database: {
-                version: databaseVersionValue,
-                max_connections: parseInt(databaseMaxConnectionsValue),
-                open_connections: databaseOpenedConnectionsValue,
-            }
-        }
-    });
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
