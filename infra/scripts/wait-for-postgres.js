c54ce333ae8faf23350011cd278de265414d4232
wellitonklein <welliton.fokushima@gmail.com>
2024-05-28 18:43:54 -0300

    adds `wait-for-postgres.js` scripts

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,22 @@
+const { exec } = require("node:child_process")
+
+
+function checkPostgres() {
+    exec('docker exec postgres-dev pg_isready --host localhost', handleReturn);
+
+    function handleReturn(error, stdout) {
+        if (stdout.search("accepting connections") == -1) {
+            process.stdout.write(".");
+            checkPostgres();
+            return;
+        }
+
+        process.stdout.write(" 😁]\n");
+        console.log("\n🟢 Postgres está pronto e aceitando conexões!")
+    }
+}
+
+console.log("🔴 Aguardando Postgres aceitar conexões\n");
+process.stdout.write("[😔 ");
+
+checkPostgres();
